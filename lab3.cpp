98ae6b100165a8bd5bbadb0e4d2921956d1ff251
maximkin22 <136425708+maximkin22@users.noreply.github.com>
2025-09-13 17:16:58 +0300

    commit lab3

--- lab3.cpp
+++ lab3.cpp
@@ -1,4 +1,3 @@
-#include <string>
 #include <iostream>
 using namespace std;
 
@@ -11,18 +10,97 @@ void int_arr_comp(int arr[], int N){
     }
 }
 
+void print_arr(int arr[], int N){
+  
+    for (int i = 0; i < N; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void copy_arr(int arr1[], int arr2[], int n){
+
+    for(int i = 0; i < n; i++){
+        arr1[i] = arr2[i];
+    }
+}
+
 
 int main() {
-
+    
     // Перемешайте книжки
     int n, k, m;
     cin >> n;
     cin >> k;
     cin >> m;
 
-    int start_arr[n];
+    int* start_arr =new int[n];
 
     int_arr_comp(start_arr, n);
 
+    // print_arr(start_arr, n);
+
+
+    for (int i = 0; i < m; i++){
+
+        int* newArr =new int[n];
+
+        for(int j = 0; j < n; j++){
+
+        if (j < n - k) {
+            newArr[j] = start_arr[j + k];
+        } else {
+            newArr[j] = start_arr[j - (n - k)];
+        }
+        }
+        copy_arr(start_arr, newArr, n);
+        delete [] newArr;
+    }
+    print_arr(start_arr, n);
+    delete[] start_arr;
+    
+
+
+    // Считалочка
+    int n, k;
+    cin >> n;
+    cin >> k;
+    if (n < 2 || n > 10000 || k < 1 || k > 100){return 0;}
+
+    int* peoples = new int[n];
+    int_arr_comp(peoples, n);
+    
+    int index = 0;
+    k = k -1;
+
+    while (n > 1)
+    {
+        int* newArr =new int[n - 1];
+
+        int index_loha = (index + k) % n;
+
+        for (int i = 0; i < index_loha; i++) {
+            newArr[i] = peoples[i];           
+        }
+        for (int i = index_loha + 1; i < n; i++) {
+            newArr[i-1] = peoples[i];        
+        }
+        
+        n--;
+
+        if (index_loha == n) {
+            index = 0;
+        } else {
+            index = index_loha;
+        }
+
+        delete[] peoples; 
+        // print_arr(newArr, n);
+        peoples = newArr;
+    }
+    
+    print_arr(peoples, n);
+    
+    delete[] peoples;
     return 0;
 }
