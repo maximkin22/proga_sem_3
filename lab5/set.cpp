05ef1ff198d8f4a2f67ed1c099ba404359490ccb
maximkin22 <136425708+maximkin22@users.noreply.github.com>
2025-10-01 18:01:26 +0300

    рабочий

--- lab5/set.cpp
+++ lab5/set.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
@@ -9,244 +10,241 @@ private:
 
 class HashMap {
 private:
-    struct Entry {
-        T value;
-        Entry(const T& v) : value(v) {}
-    };
+    static const int SizeHashMap = 200;
 
-    static const int SizeHashMap = 24;
-
-    vector<vector<Entry>> buckets;
+    vector<vector<T>> buckets;
+    
+    mutable vector<T> cached_values;
+    mutable bool cache_valid = false;
+    mutable size_t total_count = 0;  
 
 public:
-    HashMap() : buckets(SizeHashMap) {}
+    HashMap() : buckets(SizeHashMap), cache_valid(false), total_count(0) {}
 
     // хеш функция 
-    int HashIndex(const T& value) const {
-        int h = static_cast<int>(value) % SizeHashMap;
-        if (h < 0) h += SizeHashMap;
-        return h;
+    int HashIndex(const T& Tvalue) const {
+        const unsigned char* Cvalue = reinterpret_cast<const unsigned char*>(&Tvalue);
+        size_t size = sizeof(T); 
+        
+        unsigned int hash = 0;
+        for (size_t i = 0; i < size; ++i) {
+            hash = hash * 31 + Cvalue[i];  
+        }
+        
+        return (hash % SizeHashMap);
     }
-
 
     // добавление записей в таблицу
-    void InsertEntry(const T& value) {
+    void InsertValues(const T& value) {
         int bucketIndex = HashIndex(value);      
 
-        if (!(buckets[bucketIndex].empty())){
-          for (int i = 0; i < buckets[bucketIndex].size(); i++){
-            if( buckets[bucketIndex][i].value == value ) return;
-          }
+        for (int i = 0; i < buckets[bucketIndex].size(); i++) {
+            if (buckets[bucketIndex][i] == value) return;
         }
-        Entry newEntry(value);
 
-        buckets[bucketIndex].push_back(newEntry);
+        buckets[bucketIndex].push_back(value);
+        total_count++;
+        cache_valid = false; 
     }
+    
     // удалить запись
-    void DeleteEntry(const T& value) {
+    void DeleteValues(const T& value) {
         int bucketIndex = HashIndex(value);      
 
-        if (!(buckets[bucketIndex].empty())){
-          for (int i = 0; i < buckets[bucketIndex].size(); i++){
-            if( buckets[bucketIndex][i].value == value ){ 
-              buckets[bucketIndex].erase(buckets[bucketIndex].begin() + i);
-              return;
+        for (int i = 0; i < buckets[bucketIndex].size(); i++) {
+            if (buckets[bucketIndex][i] == value) { 
+                buckets[bucketIndex].erase(buckets[bucketIndex].begin() + i);
+                total_count--;
+                cache_valid = false; 
+                return;
             }
-          }
         }
     }
+    
     // проверка наличия записи
-    bool ContainsEntry(const T& value){
+    bool ContainsValues(const T& value) const {
         int bucketIndex = HashIndex(value);      
 
-        if (!(buckets[bucketIndex].empty())){
-          for (int i = 0; i < buckets[bucketIndex].size(); i++){
-            if( buckets[bucketIndex][i].value == value ) return true;
-          }
+        for (int i = 0; i < buckets[bucketIndex].size(); i++) {
+            if (buckets[bucketIndex][i] == value) return true;
         }
         return false;
     }
-    // полуить все записи (для копирования)
-    vector<T> GetAllEntry()const{
-      vector<T> result;
-      for (int i = 0; i < buckets.size(); i++)
-      {
-        for (int j = 0; j < buckets[i].size(); j++)
-        {
-          result.push_back(buckets[i][j].value);
+    
+    // получить все записи 
+  const vector<T>& GetAllValues() const {
+    if (cache_valid == false) {
+        size_t total_size = 0;
+        for (int i = 0; i < buckets.size(); i++) {
+            total_size += buckets[i].size();
         }
-      }
-      return result;
+        
+        // Обновляем кэш
+        cached_values.clear();
+        cached_values.reserve(total_size);
+        
+        for (int i = 0; i < buckets.size(); i++) {
+            for (int j = 0; j < buckets[i].size(); j++) {
+                cached_values.push_back(buckets[i][j]);
+            }
+        }
+        
+        cache_valid = true;
     }
+    return cached_values;
+  }
+    
+    size_t GetSize() const {
+        return total_count; 
+    }
+    
 };
 
   HashMap HashTable;
 
-  void insert(const T& value){
-    HashTable.InsertEntry(value);
+  void insert(const T& value) {
+    HashTable.InsertValues(value);
   }
 
-  void remove (const T& value){
-    HashTable.DeleteEntry(value);
+  void remove(const T& value) {
+    HashTable.DeleteValues(value);
   }
 
-  bool contains(const T& value){
-    return HashTable.ContainsEntry(value);
+  bool contains(const T& value) const {
+    return HashTable.ContainsValues(value);
   }
 
-  vector<T> getterAll()const{
-    return HashTable.GetAllEntry();
+  const vector<T>& getterAll() const {  
+    return HashTable.GetAllValues();
   }
 
 public:
 
-  Set(){}
+  Set() {}
 
-  Set(const Set &s){
-    vector<T> values = s.getterAll();
-    for (int i = 0; i < values.size(); i++){
-      insert(values[i]);
+  Set(const Set &s) {
+    const auto& values = s.getterAll();  
+    for (const auto& val : values) {
+      insert(val);
     }
   }
 
   //вставка элемента в множество
-  Set operator + (const T &t) const{
+  Set operator + (const T &t) const {
     Set result = *this;
     result.insert(t);
     return result;
   }
+  
   //Set<T> result = 42 + Set<T> others
-  friend Set operator + (const T &t, const Set &s){
+  friend Set operator + (const T &t, const Set &s) {
     Set newSet = s;
     newSet.insert(t);
     return newSet;
   }
 
-Set &operator += (const T &t){
+  Set &operator += (const T &t) {
     insert(t);    
     return *this; 
-}
+  }
 
   //удаление элемента из множества
-  Set operator - (const T &t) const{
+  Set operator - (const T &t) const {
     Set result = *this;
     result.remove(t);
     return result;
   }
-  Set &operator -= (const T &t){
+  
+  Set &operator -= (const T &t) {
     remove(t);
     return *this;   
   }
   
   //проверка наличия элемента в множестве
-  bool exists(const T &t) const{
-    Set result = *this;
-    return result.contains(t);
+  bool exists(const T &t) const {
+    return contains(t); 
   }
 
   //объединение множеств
-  Set operator + (const Set &s) const{
+  Set operator + (const Set &s) const {
     Set<T> result = *this;
-    vector<T> values = s.getterAll();
-    for (int i = 0; i < values.size(); i++){
+    const auto& values = s.getterAll(); 
+    for (int i = 0; i < (int)values.size(); i++) {
       result.insert(values[i]);
     }
     return result;
   }
-  Set& operator += (const Set &s){
-    vector<T> values = s.getterAll();
-    for (int i = 0; i < values.size(); i++){
+
+  Set& operator += (const Set &s) {
+    const auto& values = s.getterAll();  
+    for (int i = 0; i < (int)values.size(); i++) {
       insert(values[i]);
     }
     return *this;
   }
-
   //разность множеств
-  Set operator - (const Set &s) const{
+  Set operator - (const Set &s) const {
     Set<T> result = *this;
-    vector<T> values = s.getterAll();
-    for (int i = 0; i < values.size(); i++){
+    const auto& values = s.getterAll();  
+    for (int i = 0; i < (int)values.size(); i++) {
       result.remove(values[i]);
     }
     return result;
   }
-  Set& operator -= (const Set &s){
-    vector<T> values = s.getterAll();
-    for (int i = 0; i < values.size(); i++){
+
+  Set& operator -= (const Set &s) {
+    const auto& values = s.getterAll();  
+    for (int i = 0; i < (int)values.size(); i++) {
       remove(values[i]);
     }
     return *this;
   }
 
   //операции сравнения
-  bool operator == (const Set &s) const{
-    return getterAll() == s.getterAll();
+  bool operator == (const Set &s) const {
+    const auto& v1 = getterAll();
+    const auto& v2 = s.getterAll();
+    if (v1.size() != v2.size()) return false; 
+    
+    vector<T> sorted_v1 = v1;
+    vector<T> sorted_v2 = v2;
+    sort(sorted_v1.begin(), sorted_v1.end());
+    sort(sorted_v2.begin(), sorted_v2.end());
+    return sorted_v1 == sorted_v2;
   }
-  bool operator != (const Set &s) const{
-    return getterAll() != s.getterAll();
+  
+  bool operator != (const Set &s) const {
+    return !(*this == s);
   }
 
   //мощность множества (число элементов)
-  int size(void) const{
-    return getterAll().size();
+  int size(void) const {
+    return HashTable.GetSize(); 
   }
 
   //вывод элементов в порядке возрастания, разделяя их пробелом
   friend ostream& operator << (ostream& os, const Set &s) {
-
-    vector<T> values = s.getterAll(); 
+    vector<T> values = s.getterAll();  
 
     if (!values.empty()) {
-        for (auto it = values.begin(); it != values.end() - 1; ++it) {
-            auto minIt = it;
-            for (auto it2 = it + 1; it2 != values.end(); ++it2) {
-                if (*it2 < *minIt) {
-                    minIt = it2;
-                }
-            }
-            swap(*it, *minIt);
-        }
+        sort(values.begin(), values.end());
 
-        for (size_t i = 0; i < values.size(); ++i) {
-            if (i > 0) os << " ";
-            os << values[i];
+        os << values[0];
+        for (size_t i = 1; i < values.size(); ++i) {
+            os << " " << values[i];
         }
     }
     return os;
-  
-}
+  }
 
   //операция присваивания (если нужна)
-  Set &operator = (const Set &s){
+  Set &operator = (const Set &s) {
     if (this == &s) return *this;
     HashTable = s.HashTable;
     return *this;
-    }
+  }
     
-
-  ~Set(){}
+  ~Set() {}
 };
 
-// #include "set-test.h"  
-
-int main(){
-  Set<int> a;
-  for (int i=50; i>=0; i--) a+=i;
-  for (int i=0; i<=50; i+=2) a-=i;
-  for (int i=0; i<=50; i+=3) a-=i;
-  cout << a << endl;
-  Set<int> b = ((a + 999)+9999) - 89;
-  cout << b.size() << endl;
-  cout << b.exists(1) << b.exists(2)
-    << b.exists(3) << b.exists(4)
-    << b.exists(5) << endl;
-  Set<int> *c = new Set<int>;
-  *c +=1; *c +=5; *c +=6;
-  cout << c->size() << endl;
-  cout << a-*c << endl;
-  *c += 1111;
-  cout << a+*c << endl;
-  delete c;
-
-  return 0;
-}
+#include "set-test.h"  
